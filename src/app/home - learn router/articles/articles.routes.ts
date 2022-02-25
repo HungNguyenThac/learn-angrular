d94778ffbf7f1e3eb534fe71581cac86e39c8e5a
ThacHungNguyen <hungnt@lienviettech.com.vn>
2022-02-25 19:38:54 +0700

    UD

--- src/app/home - learn router/articles/articles.routes.ts
+++ src/app/home - learn router/articles/articles.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ArticlesListComponent } from './articles-list/articles-list.component';
 import { ArticleDetailComponent } from './child/articlesDetail.component';
 export const articlesRoutes: Routes = [
+  {},
   {
     path: '',
     component: ArticlesListComponent,
