a068e0f981741d3125909c49cf7ac1b9bd99e8f1
ThacHungNguyen <hungnt@lienviettech.com.vn>
2022-02-25 19:33:02 +0700

    UD learn lazyload component

--- src/app/home - learn router/articles/articles.routes.ts
+++ src/app/home - learn router/articles/articles.routes.ts
@@ -3,16 +3,11 @@ import { ArticlesListComponent } from './articles-list/articles-list.component';
 import { ArticleDetailComponent } from './child/articlesDetail.component';
 export const articlesRoutes: Routes = [
   {
-    path: 'articles',
-    children: [
-      {
-        path: '',
-        component: ArticlesListComponent,
-      },
-      {
-        path: ':slug',
-        component: ArticleDetailComponent,
-      },
-    ],
+    path: '',
+    component: ArticlesListComponent,
+  },
+  {
+    path: ':slug',
+    component: ArticleDetailComponent,
   },
 ];
